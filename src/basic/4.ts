36eb5ea212d9e065b1ea1efd078c1310247deb61
VadimTrubay <vadnetvadnet@ukr.net>
2024-05-02 07:56:44 +0300

    feat: add 6 files basic module

---
+++ src/basic/4.ts
@@ -0,0 +1,28 @@
+// У вас є такі функції JavaScript:
+
+// function showMessage(message) {
+//     console.log(message);
+// }
+
+// function calc(num1, num2) {
+//     return num1 + num2;
+// }
+
+// function customError() {
+//     throw new Error('Error');
+// }
+// Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
+
+function showMessage(message: string): void {
+    // @ts-ignore
+    console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+function customError(): never {
+    // @ts-ignore
+    throw new Error('Error');
+}
